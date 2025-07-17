cdfb920242c146de10ceea765f1ab606041c9533
Ramashis2004 <dasramashsiskalyan@gamil.com>
2025-07-18 00:01:27 +0530

    More abouut function

--- 04_Days/03_basics/01_function.js
+++ 04_Days/03_basics/01_function.js
@@ -40,6 +40,47 @@ function loginUserMessage(username){
   return `${username  }just logged in`
 }
 // console.log(loginUserMessage(""));
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculatinCardPrice(val1,val2,  ...num1){            //... is call ed rest oprator
+ // console.log(val1);
+  
+   return num1
+}
+//console.log(calculatinCardPrice(200,400,500,200));
+//console.log("val1");
+
+
+// const user={
+//   username: "ram",
+//   age:434
+
+
+// }
+
+// function handleObject(anyobject){
+//   console.log(`username is ${anyobject.username} and the age is ${anyobject.age}`);
+  
+// }
+// handleObject(user)
+
+
+const user = {   //why you use retuen clear explanantion
+  username: "ram",
+  age: 434
+};
+
+function handleObject(anyobject) {
+  return `username is ${anyobject.username} and the age is ${anyobject.age}`;
+}
+
+// ✅ Reuse the returned value
+const message = handleObject(user);
+
+// ✅ Use it in multiple places
+console.log("Log 1:", message);
+console.log("Log 2:", message.toUpperCase());
+console.log("Log 3:", message.length);
+
 
 
